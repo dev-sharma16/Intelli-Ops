d06ed0bc788f3cb5139610033360c17fc43af9df
Dev Sharma <devsharma1683@gmail.com>
2025-10-24 10:47:15 +0530

    added project id and some extra function(info, warn, error, critical)

--- backend/intelliops-logs-sdk/src/logger.ts
+++ backend/intelliops-logs-sdk/src/logger.ts
@@ -8,28 +8,35 @@ interface LogPayload {
   meta?: Record<string, any>;
   environment?: 'production' | 'staging' | 'development';
 }
+
 //todo : add real backend link for receiving the logs.
-const LOG_SERVICE_URL = 'https://logservice.intelliops.com/logs';
+const LOG_SERVICE_URL = 'http://localhost:3001/api/logs';
 
 export class IntelliOpsLogger {
   private apiKey: string;
+  private projectId: string;
 
-  constructor(apiKey: string) {
+  constructor(apiKey: string, projectId: string) {
     if (!apiKey) throw new Error('API key is required');
+    if (!projectId) throw new Error("Project ID is required");
     this.apiKey = apiKey;
+    this.projectId = projectId;
   }
 
   async sendLog(payload: LogPayload) {
     try {
       const logData = {
         ...payload,
+        projectId: this.projectId,
         source: payload.source || 'unknown',
-        environment: payload.environment || 'production',
         timestamp: new Date().toISOString(),
       };
 
       const response = await axios.post(LOG_SERVICE_URL, logData, {
-        headers: { 'x-api-key': this.apiKey },
+        headers: { 
+          "x-api-key": this.apiKey,
+          "Content-Type": "application/json", 
+        },
       });
 
       console.log('Log sent:', response.data);
@@ -38,5 +45,25 @@ export class IntelliOpsLogger {
       console.error('Failed to send log:', error.message);
       throw error;
     }
+  }
+
+  info(message: string, options: Partial<LogPayload> = {}) {
+    const { projectId, ...rest } = options;
+    return this.sendLog({ level: "info", message, projectId: this.projectId, ...rest });
+  }
+
+  warn(message: string, options: Partial<LogPayload> = {}) {
+    const { projectId, ...rest } = options;
+    return this.sendLog({ level: "warn", message, projectId: this.projectId, ...rest });
+  }
+
+  error(message: string, options: Partial<LogPayload> = {}) {
+    const { projectId, ...rest } = options;
+    return this.sendLog({ level: "error", message, projectId: this.projectId, ...rest });
+  }
+
+  critical(message: string, options: Partial<LogPayload> = {}) {
+    const { projectId, ...rest } = options;
+    return this.sendLog({ level: "critical", message, projectId: this.projectId, ...rest });
   }
 }
