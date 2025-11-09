3a8a27a830c882d11164d4e275c73209136c5cec
Dev Sharma <devsharma1683@gmail.com>
2025-11-09 22:03:29 +0530

    changed the routes order in auth service for fixing issue in calling project api's

--- backend/auth-service/src/routes/auth.routes.ts
+++ backend/auth-service/src/routes/auth.routes.ts
@@ -7,53 +7,19 @@ import { verifyServiceKey } from "../middlewares/serviceAuth.middleware"
 const router = Router();
 
 //* Auth Routes
-router.post(
-  '/register',
-  validators.registerValidationRules,
-  authControllers.registerUser
-);
-
-router.post(
-  '/login',
-  validators.loginValidationRules,
-  authControllers.loginUser
-);
-
+router.post('/register', validators.registerValidationRules, authControllers.registerUser);
+router.post('/login', validators.loginValidationRules, authControllers.loginUser);
 router.get('/', authMiddleware, authControllers.getCurrentUser);
 
+//* Project Routes (specific first)
+router.post('/project', authMiddleware, authControllers.createProject);
+router.get('/project', authMiddleware, authControllers.getAllProjects);
+router.get('/project/:projectId', authMiddleware, authControllers.getProjectById);
+router.delete('/project/:projectId', authMiddleware, authControllers.deleteProject);
+
+//* Other Routes (generic last)
 router.get('/:userId', verifyServiceKey, authControllers.getUserById);
-
-router.post(
-  '/logout',
-  authMiddleware,
-  authControllers.logoutUser
-)
-
+router.post('/logout', authMiddleware, authControllers.logoutUser);
 router.post('/verifyApiKey', verifyServiceKey, authControllers.verifyApiKey);
-
-//* Project Routes
-router.post(
-  '/poject',
-  authMiddleware,
-  authControllers.createProject
-)
-
-router.get(
-  '/project',
-  authMiddleware,
-  authControllers.getAllProjects
-)
-
-router.get(
-  '/project/:projectId',
-  authMiddleware,
-  authControllers.getProjectById
-)
-
-router.delete(
-  '/project/:projectId',
-  authMiddleware,
-  authControllers.deleteProject
-)
 
 export default router;
